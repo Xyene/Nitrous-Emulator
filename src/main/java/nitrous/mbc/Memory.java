92d4bfe33f90092755c5e2307e3e4e451bd5eef2
Tudor <tbrindus@gmail.com>
2015-10-25 02:00:26 -0400

    Add memory map

--- src/main/java/nitrous/mbc/Memory.java
+++ src/main/java/nitrous/mbc/Memory.java
@@ -7,6 +7,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * Represents the memory of a Gameboy.
+ * <p>
+ * The Gameboy has a 16-bit address bus, allowing it to use $0000-FFFF. Some parts of this memory range
+ * are switchable, and may either be handled by the Gameboy (e.g., work ram on the Gameboy Color), or the
+ * game cartridge (e.g., ROM banking).
+ * </p>
+ * General Memory Map
+ * 0000-3FFF   16KB ROM Bank 00     (in cartridge, fixed at bank 00)
+ * 4000-7FFF   16KB ROM Bank 01..NN (in cartridge, switchable bank number)
+ * 8000-9FFF   8KB Video RAM (VRAM) (switchable bank 0-1 in CGB Mode)
+ * A000-BFFF   8KB External RAM     (in cartridge, switchable bank, if any)
+ * C000-CFFF   4KB Work RAM Bank 0 (WRAM)
+ * D000-DFFF   4KB Work RAM Bank 1 (WRAM)  (switchable bank 1-7 in CGB Mode)
+ * E000-FDFF   Same as C000-DDFF (ECHO)    (typically not used)
+ * FE00-FE9F   Sprite Attribute Table (OAM)
+ * FEA0-FEFF   Not Usable
+ * FF00-FF7F   I/O Ports
+ * FF80-FFFE   High RAM (HRAM)
+ * FFFF        Interrupt Enable Register
+ */
 public class Memory
 {
     /**
