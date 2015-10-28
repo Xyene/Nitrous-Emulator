15616c2a55321cad1bffc48694bbf6250e7999d1
Tudor <tbrindus@gmail.com>
2015-10-28 01:02:09 -0400

    Add dummy MBC2 chip

---
+++ src/main/java/nitrous/mbc/MBC2.java
@@ -0,0 +1,15 @@
+package nitrous.mbc;
+
+import nitrous.Emulator;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import static java.lang.Math.max;
+
+public class MBC2 extends Memory
+{
+    public MBC2(Emulator core)
+    {
+        super(core);
+        throw new NotImplementedException();
+    }
+}
