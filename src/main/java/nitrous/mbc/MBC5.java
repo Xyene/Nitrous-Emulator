f1142594b82e324f2a379d30af4284b7514cebf6
Tudor <tbrindus@gmail.com>
2015-11-02 14:35:24 -0500

    Add dummy MBC5 implementation

---
+++ src/main/java/nitrous/mbc/MBC5.java
@@ -0,0 +1,15 @@
+package nitrous.mbc;
+
+import nitrous.Emulator;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import static java.lang.Math.max;
+
+public class MBC5 extends Memory
+{
+    public MBC5(Emulator core)
+    {
+        super(core);
+        throw new NotImplementedException();
+    }
+}
