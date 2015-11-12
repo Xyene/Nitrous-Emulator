daa82c14da99f29cde78a7ef8158e0530b0cae79
Tudor <tbrindus@gmail.com>
2015-11-12 01:34:44 -0500

    Fix HALT

--- src/main/java/nitrous/Emulator.java
+++ src/main/java/nitrous/Emulator.java
@@ -415,6 +415,8 @@ public class Emulator
             ac += delta;
             executed += delta;
 
+            updateInterrupts(delta);
+
             if (interruptsEnabled)
             {
                 fireInterrupts();
