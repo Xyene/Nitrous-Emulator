b52f79e467bd7231890afc9f4f31fb3028bdd014
Tudor <tbrindus@gmail.com>
2015-11-30 16:43:55 -0500

    Fix a number of issues with SquareWaveChannel
    - clock resetting on period change
    - memory updating only at the end of a cycle
    - correct duty
    - envelope sweep should not be doubled

--- src/main/java/nitrous/sound/SquareWaveChannel.java
+++ src/main/java/nitrous/sound/SquareWaveChannel.java
@@ -16,14 +16,14 @@ public class SquareWaveChannel extends SoundChannel
     private final boolean sweep;
 
     private int gbFreq;
-    private int period = 4096;
-    private int length = 0;
-    private boolean useLength = true;
+    public int period = -1;
+    public int length = 0x400000;
+    public boolean useLength = false;
     private long clockStart = 0;
-    private int envelopeInitial = 15;
+    public int envelopeInitial = 15;
     private boolean envelopeIncrease = true;
     private int envelopeSweep = 0;
-    private int duty = 6;
+    public int duty = 6;
 
     public SquareWaveChannel(Emulator core, int ioStart, boolean sweep)
     {
@@ -33,12 +33,13 @@ public class SquareWaveChannel extends SoundChannel
         this.sweep = sweep;
     }
 
-    public void update()
+    public int ___ = 0;
+
+    public void handleUpdateRequest()
     {
+        updateRequest = false;
         byte[] registers = core.mmu.registers;
 
-        if ((registers[R.R_NR24] & 0x80) != 0)
-           restart();
         /**
          * Duty   Waveform    Ratio  Cycle
          * -------------------------------
@@ -47,23 +48,104 @@ public class SquareWaveChannel extends SoundChannel
          * 2      10000111    50%        3
          * 3      01111110    75%        1
          */
-        duty = dutyConvert[(registers[ioStart] >> 6) & 0x3];
+        duty = (registers[ioStart] >> 6) & 0x3;//dutyConvert[(registers[ioStart] >> 6) & 0x3];
 
         length = (64 - (registers[ioStart] & 0x3F)) * core.clockSpeed / 256;
-        envelopeInitial = (registers[ioStart + 1] >> 4) & 0xF;
+
+
+        int xy = (registers[ioStart + 1] >> 4) & 0xF;
+
+        if (envelopeInitial != xy)
+        {
+            System.out.println("Changed volume to " + xy);
+//            clockStart = core.cycle;
+            envelopeInitial = xy;
+            currentVolume = -1;
+        }
+
         envelopeIncrease = (registers[ioStart + 1] & 0x8) != 0;
         envelopeSweep = (registers[ioStart + 1] & 0x7) * core.clockSpeed / 64;
 
-        gbFreq = (registers[ioStart + 2] & 0xFF) |
+        int _gbFreq = (registers[ioStart + 2] & 0xFF) |
                 ((registers[ioStart + 3] & 0x7) << 8);
-        period = gbFreqToCycles(gbFreq);
+
+        int n = gbFreqToCycles(_gbFreq);
+        if (period != n)
+        {
+            clockStart = core.cycle;
+            shortC = true;
+        }
+        period = n;
+        core.sound.channel2.___++;
+        core.sound.channel2.___ %= 10;
+//        System.out.printf("%speriod=%d, length=%d, duty=%d, volume=%d, clock=%d\n", (core.sound.channel2.___ == 0) ? "*" : " ",
+//                core.sound.channel2.period, core.sound.channel2.useLength ? core.sound.channel2.length : -1, core.sound.channel2.duty,
+//                core.sound.channel2.envelopeInitial, core.cycle);
+//        if(Math.abs(n-period) > 64) period = n;
+
+
+        /**
+         * period=14272, length=16384, duty=1
+         * period=14304, length=16384, duty=1
+         */
+
+//        int diff = Math.abs(period - newPeriod);
+
+//        if((newPeriod & 0xff) == 0x40) period = newPeriod;
+//        if((newPeriod & 0x7f) == 0x20) period = newPeriod;
+
+//        if (diff > 64)
+//        {
+//            period = newPeriod;
+//            gbFreq = _gbFreq;
+//        }
+//        period = newPeriod;
+//        period = newPeriod;
+        //else System.out.println(gbFreq + " ---> " + _gbFreq + " with a sweep of " + envelopeSweep);
+
+
+//        if(period == 12032) period = 12000;
+//        if(period == 10688) period = 10720;
+//        if(period == 8032) period = 80aaxaa00;
+//        if(period == 14272) period = 14304;
+//        if(period == 10112) period = 10080;
+//                     0b100000
+//        if((period & 0b111111) == 0b100000)
+//         period = period & ~0b100000;
+//        if((period & 0b1111111) == 0b1000000)
+//         period = period & ~0b1000000;
+
+//        int x= 0b10100111000000;
+
+
+//        System.out.println(period);
 
         useLength = (registers[ioStart + 3] & 0x40) != 0;
     }
 
-    public void restart()
+    boolean updateRequest = false;
+
+    public void update()
+    {
+        updateRequest = true;
+        requestedClockStart = core.cycle;
+    }
+
+    boolean restartRequest = false;
+    long requestedClockStart;
+
+    public void handleRestartRequest()
     {
         clockStart = core.cycle;
+        restartRequest = false;
+        lastCycle = -1;
+    }
+
+    boolean shortC;
+
+    public void restart()
+    {
+        restartRequest = true;
 
         /*System.out.println("Note:");
         System.out.printf("Frequency: %d, %fHz, %d, %fHz\n", gbFreq, 131072.0 / (2048 - gbFreq), period, 4194304.0 / period);
@@ -71,21 +153,79 @@ public class SquareWaveChannel extends SoundChannel
         System.out.println();*/
     }
 
+    int currentVolume = -1;
+
+    int lastCycle = -1;
+
     @Override
     public int render()
     {
         int delta = (int) (core.cycle - clockStart);
         if (useLength && delta > length)
+        {
             return 0;
+        }
 
-        int volume;
+        if (currentVolume == -1) currentVolume = envelopeInitial;
+
+        int amplitude;
         if (envelopeSweep == 0)
-            volume = envelopeInitial;
-        else
-            volume = Math.min(15, Math.max(0, envelopeInitial + delta / envelopeSweep * (envelopeIncrease ? 1 : -1))) * 2;
-        int cycle = (delta * 8 / period) & 7;
+        {
+            amplitude = currentVolume;//envelopeInitial;
+        } else
+        {
+//            if ((delta / envelopeSweep) > 1)
+//                System.out.println((delta / envelopeSweep) + " envelope cycles with delta=" + delta + ", envelopeSweep=" + envelopeSweep);
+            currentVolume = amplitude = Math.min(15, Math.max(0, envelopeInitial + delta / envelopeSweep * (envelopeIncrease ? 1 : -1)));
+            //new RuntimeException().printStackTrace();
+        }
 
-        return cycle > 1 ? volume : -volume;
+//        if(true) {
+//            System.out.println(delta + "----" + envelopeSweep);
+//        }
+
+        int cycle = (delta * 8 / period) & 7;
+        if(lastCycle == -1) lastCycle = cycle;
+
+        int diff = (Math.abs(cycle - lastCycle));
+//        if(diff > 1 && diff != 7) System.out.println("you .... " + diff);
+        lastCycle = cycle;
+
+        if (cycle == 0)
+        {
+            if (updateRequest) handleUpdateRequest();
+            if (restartRequest)
+            {
+                handleRestartRequest();
+            }
+//            if(shortC)
+//            {
+//                shortC = false;
+//                return 200;
+//            }
+        }
+        /**
+         * Duty   Waveform    Ratio  Cycle
+         * -------------------------------
+         * 0      00000001    12.5%      6
+         * 1      10000001    25%        5
+         * 2      10000111    50%        3
+         * 3      01111110    75%        1
+         */
+        switch (duty)
+        {
+            case 0:
+                return (byte) (cycle == 7 ? amplitude * 2 : 0);
+            case 1:
+                return (byte) (cycle == 0 || cycle == 7 ? amplitude * 2 : 0);
+            case 2:
+                return (byte) (cycle == 0 || cycle > 4 ? amplitude * 2 : 0);
+            case 3:
+                return (byte) (cycle != 0 && cycle != 7 ? amplitude * 2 : 0);
+        }
+        return -1;
+
+//        return cycle > (duty-1) ? 2 * amplitude : 0;
     }
 
 //    public void render(byte[] output, int off, int len)
