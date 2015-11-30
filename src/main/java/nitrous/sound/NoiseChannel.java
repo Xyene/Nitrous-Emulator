3d6558bb7d87527e80a50843344a2a875e428dcd
Quantum <quantum@dmoj.ca>
2015-11-30 13:16:40 -0500

    Add noise channel that seems to work really well

---
+++ src/main/java/nitrous/sound/NoiseChannel.java
@@ -0,0 +1,109 @@
+package nitrous.sound;
+
+import nitrous.Emulator;
+
+import static nitrous.R.*;
+
+public class NoiseChannel extends SoundChannel
+{
+    private int gbFreq;
+    public int period = -1;
+    public int length = 0x400000;
+    public boolean useLength = false;
+    private long clockStart = 0;
+    public int envelopeInitial = 15;
+    private boolean envelopeIncrease = true;
+    private int envelopeSweep = 0;
+
+    public NoiseChannel(Emulator core)
+    {
+        super(core);
+    }
+
+    boolean updateRequest = false;
+    boolean restartRequest = false;
+
+    private void handleUpdateRequest()
+    {
+        updateRequest = false;
+        byte[] registers = core.mmu.registers;
+
+        length = (64 - (registers[R_NR41] & 0x3F)) * 16384;
+
+        int newEnvelope = (registers[R_NR42] >> 4) & 0xF;
+
+        if (envelopeInitial != newEnvelope)
+        {
+            envelopeInitial = newEnvelope;
+            currentVolume = -1;
+        }
+
+        envelopeIncrease = (registers[R_NR42] & 0x8) != 0;
+        envelopeSweep = (registers[R_NR42] & 0x7) * 65536;
+
+        int s = (registers[R_NR43] >> 4) & 0xF;
+        int r = registers[R_NR43] & 0x7;
+        boolean _7bits = (registers[R_NR43] & 0x8) != 0;
+
+        int freq = (r == 0 ? 1048576 : 524288 * r) >> (s + 1);
+        period = 4194304 / freq;
+
+        System.out.println(period);
+
+        useLength = (registers[R_NR44] & 0x40) != 0;
+    }
+
+    public void update() {
+        updateRequest = true;
+    }
+
+    public void handleRestartRequest()
+    {
+        clockStart = core.cycle;
+        restartRequest = false;
+    }
+
+    public void restart() {
+        restartRequest = true;
+    }
+
+    int currentVolume = -1;
+    boolean high = false;
+    long lastToggle = Integer.MIN_VALUE;
+
+    @Override
+    public int render()
+    {
+        int delta = (int) (core.cycle - clockStart);
+        if (useLength && delta > length)
+        {
+            return 0;
+        }
+
+        if (currentVolume == -1)
+            currentVolume = envelopeInitial;
+
+        int amplitude;
+        if (envelopeSweep == 0)
+        {
+            amplitude = currentVolume;
+        } else
+        {
+            currentVolume = amplitude = Math.min(15, Math.max(0, envelopeInitial + delta / envelopeSweep * (envelopeIncrease ? 1 : -1)));
+        }
+
+        if (core.cycle - lastToggle >= period)
+        {
+            if (updateRequest)
+                handleUpdateRequest();
+
+            if (restartRequest)
+                handleRestartRequest();
+
+            high ^= Math.random() >= 0.5;
+            lastToggle = core.cycle;
+        }
+
+        return high ? 2 * amplitude : 0;
+    }
+}
