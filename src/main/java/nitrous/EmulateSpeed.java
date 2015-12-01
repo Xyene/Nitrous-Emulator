b30e10723f70e00d151553127f6145f2df5fb361
Quantum <quantum2048@gmail.com>
2015-11-30 21:07:51 -0500

    Adjustable speed.
    Not everyone is patient enough to play slow gameboy games.

---
+++ src/main/java/nitrous/EmulateSpeed.java
@@ -0,0 +1,20 @@
+package nitrous;
+
+public enum EmulateSpeed {
+    SINGLE("1x", 4194304),
+    DOUBLE("2x", 8388608),
+    TRIPLE("3x", 12582912),
+    QUADRUPLE("4x", 16777216),
+    SEXTUPLE("6x", 25165824),
+    OCTUPLE("8x", 33554432),
+    DUODECUPLE("12x", 50331648),
+    HEXADECUPLE("16x", 67108864);
+
+    public final String name;
+    public final int clockSpeed;
+
+    EmulateSpeed(String name, int clockSpeed) {
+        this.name = name;
+        this.clockSpeed = clockSpeed;
+    }
+}
