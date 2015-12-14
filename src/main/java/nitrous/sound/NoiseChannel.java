d6f4c8cbc25b54776cdb13584e245cbc317466ec
Quantum <quantum2048@gmail.com>
2015-12-14 13:40:21 -0500

    Add documentation to NoiseChannel

--- src/main/java/nitrous/sound/NoiseChannel.java
+++ src/main/java/nitrous/sound/NoiseChannel.java
@@ -58,91 +58,199 @@ import static nitrous.R.*;
  */
 public class NoiseChannel extends SoundChannel
 {
-    private int gbFreq;
+    /**
+     * Time between potential toggles in the wave form.
+     *
+     * Measured in clock cycles.
+     */
     public int period = -1;
+
+    /**
+     * Length of the sound, in clock cycles.
+     */
     public int length = 0x400000;
+
+    /**
+     * Whether we should respect the {@link #length} variable, or just play forever.
+     */
     public boolean useLength = false;
+
+    /**
+     * The clock cycle that marks the start of the current sound.
+     */
     private long clockStart = 0;
+
+    /**
+     * The initial volume for envelope.
+     */
     public int envelopeInitial = 15;
+
+    /**
+     * Whether to increase or decrease the volume after {@link #envelopeSweep} cycles later.
+     */
     private boolean envelopeIncrease = true;
+
+    /**
+     * Time between volume change, measured in clock cycles.
+     */
     private int envelopeSweep = 0;
 
+    /**
+     * Whether the wave should be more regular.
+     */
+    private boolean regular;
+
+    /**
+     * Current volume field.
+     *
+     * -1 means update, hence recalculate from {@link #envelopeInitial}
+     */
+    int currentVolume = -1;
+
+    /**
+     * The current state of the waveform.
+     *
+     * {@literal true} means a positive amplitude, and {@literal false} means a negative one.
+     */
+    boolean high = false;
+
+    /**
+     * The clock cycle when the waveform was last toggled.
+     */
+    long lastToggle = Integer.MIN_VALUE;
+
+    /**
+     * Field that shows whether we are still playing the sound.
+     *
+     * i.e. whether {@link #length} is respected and exceeded.
+     */
+    public boolean isPlaying;
+
+    /**
+     * Creates a {@link NoiseChannel} class.
+     *
+     * @param core the {@link Emulator} instance
+     */
     public NoiseChannel(Emulator core)
     {
         super(core);
     }
 
+    /**
+     * Handle sound update.
+     *
+     * This method rereads everything from memory.
+     */
     @Override
     protected void handleUpdateRequest()
     {
         byte[] registers = core.mmu.registers;
 
+        // Update length.
         length = (64 - (registers[R_NR41] & 0x3F)) * 16384;
 
+        // Check if we should respect sound length.
+        useLength = (registers[R_NR44] & 0x40) != 0;
+
+        // Get envelope initial settings.
         int newEnvelope = (registers[R_NR42] >> 4) & 0xF;
 
+        // If changed, we update and reset the envelope.
         if (envelopeInitial != newEnvelope)
         {
             envelopeInitial = newEnvelope;
             currentVolume = -1;
         }
 
+        // Update envelope increase flag and sweep time.
         envelopeIncrease = (registers[R_NR42] & 0x8) != 0;
         envelopeSweep = (registers[R_NR42] & 0x7) * 65536;
 
+        // Get parameters s and r used in frequency calculation.
         int s = (registers[R_NR43] >> 4) & 0xF;
         int r = registers[R_NR43] & 0x7;
-        boolean _7bits = (registers[R_NR43] & 0x8) != 0;
 
-        int freq = (r == 0 ? 1048576 : 524288 * r) >> (s + 1);
+        // Check if the noise should be more regular.
+        regular = (registers[R_NR43] & 0x8) != 0;
+
+        // Calculate frequency.
+        int freq;
+        if (r == 0)
+        {
+            // If r == 0, we use 1048576 Hz / 2^(s+1).
+            freq = 1048576 >> (s + 1);
+        } else
+        {
+            // Otherwise we use 524288 Hz / r / 2^(s+1)
+            freq = 524288 * r >> (s + 1);
+        }
+
+        // Convert frequency to period.
         period = 4194304 / freq;
-
-        useLength = (registers[R_NR44] & 0x40) != 0;
     }
 
+    /**
+     * Handle restart request: update {@link #clockStart}.
+     */
     @Override
     protected void handleRestartRequest()
     {
         clockStart = core.cycle;
     }
 
-    int currentVolume = -1;
-    boolean high = false;
-    long lastToggle = Integer.MIN_VALUE;
-    public boolean isPlaying;
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int render()
     {
+        // Calculate the amount of clock cycles elapsed since the sound was started.
         int delta = (int) (core.cycle - clockStart);
+
+        // If this exceeds the sound length, stop playing and return no sound.
         if (useLength && delta > length)
         {
             isPlaying = false;
             return 0;
         }
+
+        // Otherwise the sound is still playing.
         isPlaying = true;
 
+        // If currentVolume == -1, we are updating the envelope, so we reset the volume to initial.
         if (currentVolume == -1)
             currentVolume = envelopeInitial;
 
+        // Calculate the amplitude of the wave.
+        // Note that our amplitude is twice as big Gameboy's since we have more bits of output.
         int amplitude;
+
         if (envelopeSweep == 0)
         {
+            // If there is no sweep, use current volume.
             amplitude = currentVolume * 2;
         } else
         {
+            // Otherwise, calculate the amount of sweeping done and predict what the envelope would be at,
+            // then capping it to fit between 0 and 15 inclusive.
             currentVolume = amplitude = Math.min(15, Math.max(0, envelopeInitial + delta / envelopeSweep * (envelopeIncrease ? 1 : -1))) * 2;
         }
 
+        // If it has been one entire period since last toggle.
         if (core.cycle - lastToggle >= period)
         {
+            // Handle update and restart requests, then restart processing.
             if (handleRequests())
                 return render();
 
+            // Toggle the waveform randomly.
             high ^= Math.random() >= 0.5;
+
+            // Update the last toggle time.
             lastToggle = core.cycle;
         }
 
+        // Return a positive amplitude if the wave is high, otherwise a negative one.
         return high ? amplitude : -amplitude;
     }
 }
