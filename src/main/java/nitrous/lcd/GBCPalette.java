35d86d111589e0ad2d18f9dfccf6b5b1668e4323
Tudor <tbrindus@gmail.com>
2015-12-15 12:10:49 -0500

    Add documentation to GBCPalette

--- src/main/java/nitrous/lcd/GBCPalette.java
+++ src/main/java/nitrous/lcd/GBCPalette.java
@@ -1,14 +1,31 @@
 package nitrous.lcd;
 
+/**
+ * Represents a Gameboy Color
+ */
 public class GBCPalette implements IPalette
 {
+    /**
+     * An array of 4 RGBA palette entries.
+     * Package-local for easier access from LCD.
+     */
     final int[] colors;
 
+    /**
+     * Constructs a new GBCPalette.
+     *
+     * @param colors The palette, in RGBA format.
+     */
     public GBCPalette(int[] colors)
     {
+        if (colors.length != 4)
+            throw new IllegalArgumentException("colors must be of length 4");
         this.colors = colors;
     }
 
+    /**
+     * @{inheritDoc}
+     */
     public int getColor(int number)
     {
         return colors[number];
