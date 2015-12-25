a7d2c89104650bf6dc0a5c9b90495633b08a494e
Tudor <tbrindus@gmail.com>
2015-12-24 21:14:31 -0500

    Add documentation to IPalette

--- src/main/java/nitrous/lcd/IPalette.java
+++ src/main/java/nitrous/lcd/IPalette.java
@@ -1,6 +1,15 @@
 package nitrous.lcd;
 
+/**
+ * A generic palette. Implementing classes include DMGPalette and GBCPalette.
+ */
 public interface IPalette
 {
+    /**
+     * Gets the RGBA color associated to a given index.
+     *
+     * @param number The color index.
+     * @return A color in RGBA format.
+     */
     int getColor(int number);
 }
