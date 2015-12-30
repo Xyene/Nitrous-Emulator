d44665bea541c69300c49046de347fa79425a5ce
Tudor <tbrindus@gmail.com>
2015-12-29 22:35:12 -0500

    Add documentation to EmulateSpeed

--- src/main/java/nitrous/EmulateSpeed.java
+++ src/main/java/nitrous/EmulateSpeed.java
@@ -1,6 +1,12 @@
 package nitrous;
 
-public enum EmulateSpeed {
+/**
+ * Definitions for supported emulation speeds.
+ * <p>
+ * All speeds are based off of a 4.194304MHz clock.
+ */
+public enum EmulateSpeed
+{
     QUARTER("0.25x", 1048576),
     HALF("0.5x", 2097152),
     SINGLE("1x", 4194304),
@@ -12,10 +18,24 @@ public enum EmulateSpeed {
     DUODECUPLE("12x", 50331648),
     HEXADECUPLE("16x", 67108864);
 
+    /**
+     * The name of the speed as show in the GUI.
+     */
     public final String name;
+
+    /**
+     * The clock speed.
+     */
     public final int clockSpeed;
 
-    EmulateSpeed(String name, int clockSpeed) {
+    /**
+     * Creates a new EmulateSpeed.
+     *
+     * @param name       The identifier to show to the user in the GUI.
+     * @param clockSpeed The clock speed.
+     */
+    EmulateSpeed(String name, int clockSpeed)
+    {
         this.name = name;
         this.clockSpeed = clockSpeed;
     }
