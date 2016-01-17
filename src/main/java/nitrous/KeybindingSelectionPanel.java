f522fa1fe43aff12c13d0cc77652c5b5e9e32d27
Quantum <quantum2048@gmail.com>
2016-01-17 17:12:42 -0500

    Add documentation to KeybindingSelectionPanel

--- src/main/java/nitrous/KeybindingSelectionPanel.java
+++ src/main/java/nitrous/KeybindingSelectionPanel.java
@@ -5,21 +5,36 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+/**
+ * A {@link JPanel} to manage keybinding selection.
+ */
 public class KeybindingSelectionPanel extends JPanel
 {
+    /**
+     * Create the Panel.
+     *
+     * @param keybinding the underlying keybinding storage
+     */
     public KeybindingSelectionPanel(Keybinding keybinding)
     {
         setLayout(new GridLayout(0, 2, 10, 0));
 
+        // Loop over all keys.
         for (int i = 0; i < keybinding.getKeyCount(); i++)
         {
+            // Use a new variable every time.
+            // Otherwise, all inner classes will see i = key count by the time the loop finishes.
             int button = i;
+
+            // Get the key name and current setting.
             String id = keybinding.getKeyName(i);
             int keystroke = keybinding.getKeystroke(i);
 
+            // Create a panel to contain the button name label and a text field to display the binding.
             add(new JPanel()
             {
                 {
+                    // THe button name label.
                     add(new JLabel(id)
                     {
                         {
@@ -27,12 +42,15 @@ public class KeybindingSelectionPanel extends JPanel
                         }
                     });
 
+                    // The binding text field.
                     add(new JTextField(10)
                     {
                         {
+                            // Show the name of the current bound key.
                             setForeground(getDisabledTextColor());
                             setText(KeyEvent.getKeyText(keystroke));
 
+                            // Event handler to update biding.
                             addKeyListener(new KeyAdapter()
                             {
                                 @Override
@@ -46,6 +64,9 @@ public class KeybindingSelectionPanel extends JPanel
                                 {
                                     e.consume();
 
+                                    // When a key is pressed while the TextField is focused,
+                                    // we attempt to bind it to the button. If this is successful
+                                    // we update the TextField to reflect the new binding.
                                     if (keybinding.updateKey(button, e.getKeyCode()))
                                         setText(KeyEvent.getKeyText(e.getKeyCode()));
                                 }
