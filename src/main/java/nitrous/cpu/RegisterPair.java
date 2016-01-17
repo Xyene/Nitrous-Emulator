8ffb751060fe90e5e8f10094a33c6c58458df405
Tudor <tbrindus@gmail.com>
2016-01-17 00:10:37 -0500

    Document RegisterPair

--- src/main/java/nitrous/cpu/RegisterPair.java
+++ src/main/java/nitrous/cpu/RegisterPair.java
@@ -1,8 +1,13 @@
 package nitrous.cpu;
 
-public enum RegisterPair
-{
+/**
+ * Defines a register pair; that is, a 16-bit value made up of two 8-bit values.
+ */
+public enum RegisterPair {
     BC, DE, HL, SP;
 
+    /**
+     * CPU id -> RegisterPair conversion.
+     */
     public static final RegisterPair[] byValue = {BC, DE, HL, SP};
 }
