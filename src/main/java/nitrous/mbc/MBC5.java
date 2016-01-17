4df345b5332c3d337ddc3f36503e035a83f99ef9
Tudor <tbrindus@gmail.com>
2016-01-17 16:06:46 -0500

    Reimplement MBC5 to make more sense

--- src/main/java/nitrous/mbc/MBC5.java
+++ src/main/java/nitrous/mbc/MBC5.java
@@ -5,90 +5,73 @@ import nitrous.cpu.Emulator;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class MBC5 extends MBC
-{
+public class MBC5 extends MBC {
     private int romBank = 1;
 
-    public MBC5(Emulator core)
-    {
+    public MBC5(Emulator core) {
         super(core);
         cartRam = new byte[RAM_PAGESIZE * 16];
     }
 
     @Override
-    public boolean hasBattery()
-    {
+    public boolean hasBattery() {
         return true;
     }
 
     @Override
-    public void save(OutputStream out) throws IOException
-    {
+    public void save(OutputStream out) throws IOException {
         out.write(cartRam);
     }
 
+    /**
+     * Maps a ROM bank to be accessed.
+     *
+     * @param bank The bank number.
+     */
+    private void mapRom(int bank) {
+        romBank = bank;
+        romPageStart = Memory.ROM_PAGESIZE * bank;
+    }
+
     @Override
-    public void setAddress(int addr, int _data)
-    {
+    public void setAddress(int addr, int _data) {
         addr &= 0xffff;
         byte data = (byte) (_data & 0xff);
-        switch (addr & 0xF000)
-        {
+        switch (addr & 0xF000) {
             case 0x0000:
             case 0x1000:
                 /**
-                 * Before external RAM can be read or written, it must be enabled by writing to this address space.
-                 * It is recommended to disable external RAM after accessing it, in order to protect its contents
-                 * from damage during power down of the gameboy. Usually the following values are used:
-                 *
-                 * 00h  Disable RAM (default)
-                 * 0Ah  Enable RAM
-                 *
-                 * Practically any value with 0Ah in the lower 4 bits enables RAM, and any other value disables RAM.
+                 * Same as for MBC1.
                  */
                 if (core.cartridge.ramBanks > 0)
                     ramEnabled = (data & 0x0F) == 0x0A;
                 break;
             case 0xA000:
             case 0xB000:
-                /**
-                 * This area is used to address external RAM in the cartridge (if any).
-                 * External RAM is often battery buffered, allowing to store game positions or high score tables,
-                 * even if the gameboy is turned off, or if the cartridge is removed from the gameboy.
-                 * Available RAM sizes are: 2KByte (at A000-A7FF), 8KByte (at A000-BFFF), and 32KByte
-                 * (in form of four 8K banks at A000-BFFF).
-                 */
-                if (ramEnabled)
-                {
+                if (ramEnabled) {
                     cartRam[addr - 0xA000 + ramPageStart] = data;
                 }
                 break;
             case 0x2000:
+                /**
+                 * The lower 8 bits of the ROM bank number goes here.
+                 * Writing 0 will indeed give bank 0 on MBC5, unlike other MBCs.
+                 */
+                mapRom((romBank & 0b100000000) | (data & 0xff));
+                break;
             case 0x3000:
-                if (addr < 0x3000)
-                {
-                    romBank = data | (currentRomBankHi << 8);
-                } else
-                {
-                    currentRomBankHi = data & 0x01;
-                    romBank = (romBank & 0xFF) | (currentRomBankHi << 8);
-                }
-                // romBank &= (core.cartridge.romBanks - 1);
-                romPageStart = romBank * 0x4000;
+                /**
+                 * The 9th bit of the ROM bank number goes here.
+                 */
+                mapRom((romBank & 0xff) | ((data & 0x1) << 8));
                 break;
             case 0x4000:
             case 0x5000:
-                ramPageStart = (data & 0x0F) * RAM_PAGESIZE;
-                break;
-            case 0x6000:
-            case 0x7000:
-                System.err.println("Invalid write!");
+                ramPageStart = (data & 0x03) * RAM_PAGESIZE;
                 break;
             default:
                 super.setAddress(addr, data);
                 break;
         }
     }
-
-    int currentRomBankHi;
 }
