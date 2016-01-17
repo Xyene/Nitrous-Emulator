1ed27afcca1124d696dbe54d2b04823b6bbaa4a5
Tudor <tbrindus@gmail.com>
2016-01-17 12:53:03 -0500

    Base EmulateSpeed off Emulator.BASE_CLOCK_SPEED

--- src/main/java/nitrous/EmulateSpeed.java
+++ src/main/java/nitrous/EmulateSpeed.java
@@ -1,22 +1,25 @@
 package nitrous;
 
+import nitrous.cpu.Emulator;
+
+import static nitrous.cpu.Emulator.BASE_CLOCK_SPEED;
+
 /**
  * Definitions for supported emulation speeds.
- * <p>
+ * <p/>
  * All speeds are based off of a 4.194304MHz clock.
  */
-public enum EmulateSpeed
-{
-    QUARTER("0.25x", 1048576),
-    HALF("0.5x", 2097152),
-    SINGLE("1x", 4194304),
-    DOUBLE("2x", 8388608),
-    TRIPLE("3x", 12582912),
-    QUADRUPLE("4x", 16777216),
-    SEXTUPLE("6x", 25165824),
-    OCTUPLE("8x", 33554432),
-    DUODECUPLE("12x", 50331648),
-    HEXADECUPLE("16x", 67108864);
+public enum EmulateSpeed {
+    QUARTER("0.25x", BASE_CLOCK_SPEED / 4),
+    HALF("0.5x", BASE_CLOCK_SPEED / 2),
+    SINGLE("1x", BASE_CLOCK_SPEED),
+    DOUBLE("2x", BASE_CLOCK_SPEED * 2),
+    TRIPLE("3x", BASE_CLOCK_SPEED * 3),
+    QUADRUPLE("4x", BASE_CLOCK_SPEED * 4),
+    SEXTUPLE("6x", BASE_CLOCK_SPEED * 6),
+    OCTUPLE("8x", BASE_CLOCK_SPEED * 8),
+    DUODECUPLE("12x", BASE_CLOCK_SPEED * 12),
+    HEXADECUPLE("16x", BASE_CLOCK_SPEED * 16);
 
     /**
      * The name of the speed as show in the GUI.
@@ -34,8 +37,7 @@ public enum EmulateSpeed
      * @param name       The identifier to show to the user in the GUI.
      * @param clockSpeed The clock speed.
      */
-    EmulateSpeed(String name, int clockSpeed)
-    {
+    EmulateSpeed(String name, int clockSpeed) {
         this.name = name;
         this.clockSpeed = clockSpeed;
     }
