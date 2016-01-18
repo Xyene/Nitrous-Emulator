a70ca016cc39fd5af4a60023e837bbaf32df949f
Quantum <quantum2048@gmail.com>
2016-01-18 00:27:45 -0500

    Use hashes to avoid long key names, which Java/Windows hates

--- src/main/java/nitrous/ROMFrequencyManager.java
+++ src/main/java/nitrous/ROMFrequencyManager.java
@@ -1,6 +1,9 @@
 package nitrous;
 
 import java.io.File;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.prefs.BackingStoreException;
@@ -12,9 +15,14 @@ import java.util.prefs.Preferences;
 public class ROMFrequencyManager
 {
     /**
-     * Backing store: with {@link Preferences}.
+     * Frequency backing store: with {@link Preferences}.
      */
     private final Preferences store;
+
+    /**
+     * File name backing store: with {@link Preferences}.
+     */
+    private final Preferences fileNameStore;
 
     /**
      * Create a ROM freqeuency manager from its backing store: {@link Preferences}.
@@ -24,6 +32,31 @@ public class ROMFrequencyManager
     public ROMFrequencyManager(Preferences store)
     {
         this.store = store;
+        fileNameStore = store.node("fileName");
+    }
+
+    /**
+     * Utility method to MD5 hash a {@link String}.
+     *
+     * @param input a string to hash
+     * @return the hexadecimal hash output
+     */
+    public static String md5String(String input) {
+        byte[] digest;
+
+        // Use Java's crypto library to hash.
+        try
+        {
+            digest = MessageDigest.getInstance("MD5").digest(input.getBytes());
+        } catch (NoSuchAlgorithmException e)
+        {
+            // This can't really happen.
+            throw new RuntimeException(e);
+        }
+
+        // Use BigInteger to convert binary data into hexadecimal.
+        BigInteger bi = new BigInteger(1, digest);
+        return String.format("%0" + (digest.length << 1) + "X", bi);
     }
 
     /**
@@ -36,8 +69,14 @@ public class ROMFrequencyManager
         // Used the absolute path to identify the file.
         String path = file.getAbsolutePath();
 
+        // Hash the string to avoid length limit
+        String hash = md5String(path);
+
+        // Store real path
+        fileNameStore.put(hash, path);
+
         // Increase the usage frequency.
-        store.putInt(path, store.getInt(path, 0) + 1);
+        store.putInt(hash, store.getInt(hash, 0) + 1);
     }//end usedROM
 
     /**
@@ -127,8 +166,18 @@ public class ROMFrequencyManager
         try
         {
             // Look at every known ROM.
-            for (String rom : store.keys())
+            for (String hash : store.keys())
             {
+                // Get the ROM file name from the hash.
+                String rom = fileNameStore.get(hash, null);
+
+                // Remove the hash entry if it somehow wasn't mapped to a name.
+                if (rom == null)
+                {
+                    store.remove(hash);
+                    continue;
+                }
+
                 // Create file object based on path.
                 File file = new File(rom);
 
@@ -136,11 +185,12 @@ public class ROMFrequencyManager
                 if (file.isFile())
                 {
                     roms.add(file);
-                    frequency.put(file, store.getInt(rom, 0));
+                    frequency.put(file, store.getInt(hash, 0));
                 } else
                 {
                     // Remove information about deleted ROMs.
-                    store.remove(rom);
+                    store.remove(hash);
+                    fileNameStore.remove(hash);
                 }//end if
             }//end for
         } catch (BackingStoreException e)
